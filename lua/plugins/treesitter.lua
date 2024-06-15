6cc7aae9c592a9c51f964647d6695cd8b2740c95
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-06-15 16:48:11 +0200

    Add treesitter text objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "windwp/nvim-ts-autotag",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })()
@@ -16,6 +17,28 @@ return {
       highlight = { enable = true },
       indent = { enable = true },
       autotag = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ii"] = "@conditional.inner",
+            ["ai"] = "@conditional.outer",
+            ["il"] = "@loop.inner",
+            ["al"] = "@loop.outer",
+            ["i/"] = "@comment.inner",
+            ["a/"] = "@comment.outer",
+            ["="] = "@assignment.rhs",
+          },
+        },
+      },
     })
   end,
 }
