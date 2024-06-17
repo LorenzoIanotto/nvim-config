4dab52de5f19813d50812a524e7f0d62b0f5ead7
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-06-17 16:31:56 +0200

    Fix trouble.nvim open command

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,12 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function(_, opts)
-    local trouble = require("trouble")
-    trouble.setup(opts)
-
-    vim.keymap.set("n", "<leader>tw", function()
-      trouble.toggle("workspace_diagnostics")
-    end, { desc = "Open Trouble workspace diagnostics" })
-  end,
-  opts = {},
+  config = true,
+  keys = {
+    {
+      "<leader>tw",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  },
 }
