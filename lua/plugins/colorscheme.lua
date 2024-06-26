a4e6157807522823533fa4a79611d81590e43e4e
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-06-26 11:38:10 +0200

    Change catppuccin config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,17 +3,40 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function(_, opts)
-    vim.cmd.colorscheme("catppuccin")
-
     require("catppuccin").setup(opts)
+
+    vim.cmd.colorscheme("catppuccin")
   end,
   opts = {
     integrations = {
       gitsigns = true,
+      cmp = true,
+      fidget = true,
+      harpoon = true,
       mason = true,
       neotree = true,
       telescope = {
         enabled = true,
+      },
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = {},
+          hints = {},
+          warnings = {},
+          information = {},
+          ok = {},
+        },
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+          ok = { "undercurl" },
+        },
+        inlay_hints = {
+          background = true,
+        },
       },
     },
   },
